6c2c716bd49f3ec88ea9d7e784fdeca39678f023
m1st3rlang <93043282+m1st3rlang@users.noreply.github.com>
2026-03-03 02:05:29 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+  });
+
+  socket.on("chatMessage", ({ room, message }) => {
+    io.to(room).emit("chatMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
